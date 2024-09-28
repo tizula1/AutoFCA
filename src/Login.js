38dafe987a608774967803d2313d76201ac1041e
Guilherme de Souza <89468702+tizula1@users.noreply.github.com>
2024-09-28 19:46:31 -0300

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
     const repoName = 'auto-fca';
     const folderName = `Registros/${matricula}`;
     const content = btoa('Este é o arquivo README para a matrícula ' + matricula);
-    const token = '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';
+    const token = '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';
     const checkFolderUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderName}`;
     const defaultFolderUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/Registros/default`;
     try {
